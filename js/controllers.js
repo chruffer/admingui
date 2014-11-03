99df7e35c28824c14085522fa665535c6909354b
Christian Ruffer <christian@fritz-payment.com>
2014-11-03 14:58:23 +0100

    prepared initial login handling structure

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,26 @@
 
 var adminControllers = angular.module('adminControllers',[]);
 
-adminControllers.controller('LoginCtrl', ['$scope', '$http',
- 	function($scope, $http){
+adminControllers.controller('LoginCtrl', ['$scope', '$http', '$log',
+ 	function($scope, $http, $log){
 
+		$scope.$log = $log
+		
 		$scope.login = function(){
-			
+			$http.get("http://localhost:8080/v1/authorization/basic").
+  				success(function(data, status, headers, config) {
+  					// @todo 
+					$log.info("success")
+				}).
+				error(function(data, status, headers, config) {
+  					// @todo 
+					$log.info("fail")
+				})
 		}
-	}]);
+}])
+
+adminControllers.controller('AdminCtrl', ['$scope', '$http', '$log',
+	function($scope, $http, $log){
+		$log.info('test')
+
+}])
