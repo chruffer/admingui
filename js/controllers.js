fdbb6bef93ddc4a73ec16973c344c65b2a79f0f4
Bart <test@test.com>
2014-11-03 15:16:35 +0100

    logout button + func

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,9 @@ adminControllers.controller('LoginCtrl', ['$scope', '$http',
  	function($scope, $http){
 
 		$scope.login = function(){
-			
-		}
+			$http.get('http://localhost:8080/v1/authorization/basic')
+		};
+		$scope.logout = function(){
+			$http.delete('http://localhost:8080/v1/authorization')
+		};
 	}]);
